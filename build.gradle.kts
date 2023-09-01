0ddf1baa95cb03de1f71d74d2e6fb4687c59a96f
mfnalex <mfnalex@jeff-media.com>
2023-09-01 13:51:43 +0200

    shadow

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,9 @@
+plugins {
+    `java-library`
+    `maven-publish`
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+}
+
 allprojects {
     repositories {
         mavenCentral()
@@ -13,4 +19,17 @@ allprojects {
             url = uri("https://repo.extendedclip.com/content/repositories/placeholderapi/")
         }
     }
+}
+
+tasks.shadowJar {
+    archiveClassifier.set("")
+}
+
+tasks.build {
+    dependsOn(tasks.shadowJar)
+}
+
+dependencies {
+    implementation(project(":papi-replacer"))
+    implementation(project(":yaml-commands"))
 }
